f9bd7a8c5434d95a9cbe79cc196f6e54ab70690b
Ryan Koval <ryan@ryankoval.com>
2023-07-31 22:55:42 -0500

    upgraded ghost

--- update-ghost-data.sh
+++ update-ghost-data.sh
@@ -5,8 +5,8 @@ tarball_name='local-ghost-data.tar.gz'
 
 rm -f $tarball_name
 tar -zvcf $tarball_name ghost-data
-scp $tarball_name centos:/Users/ryankoval/workspace/personal/infrastructure/
-ssh centos -t "cd /Users/ryankoval/workspace/personal/infrastructure && tar -xvzf $tarball_name && docker restart infrastructure_ghost_1"
+scp $tarball_name centos:/Users/ryankoval/workspace/infrastructure/
+ssh centos -t "cd /Users/ryankoval/workspace/infrastructure && tar -xvzf $tarball_name && docker compose restart ghost"
 
 echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 echo 'YOU MUST NOW REUPLOAD THE BLOGINN THEME TO GHOST'
