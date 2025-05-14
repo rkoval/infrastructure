8bb60238aec79bc57a54692630b9984680974665
Ryan Koval <ryan@ryankoval.com>
2025-05-14 15:17:07 -0500

    updated

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@ set -e
 
 
 echo "you must be \`eval\`'d into centos to build marketing-site because there's some segfault bug with docker for mac when cross building!!"
+set -x
 eval $(docker-machine env -u)
 docker login
-docker buildx bake
-docker compose push
+docker buildx bake "$@"
+docker compose push "$@"
 
 docker-machine env centos-docker-machine
 eval $(docker-machine env centos-docker-machine)
-docker compose pull
-docker compose up -d
+docker compose pull "$@"
+docker compose up -d "$@"
